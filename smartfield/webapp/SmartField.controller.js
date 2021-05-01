0068579d3f064cf60822b264d91789c1a48d50f6
wangzixi-diablo <cle.ee@163.com>
2021-05-01 19:24:53 +0800

    remove unused service

--- smartfield/webapp/SmartField.controller.js
+++ smartfield/webapp/SmartField.controller.js
@@ -6,7 +6,6 @@ sap.ui.define([
 	return Controller.extend("sap.ui.demo.smartControls.SmartField", {
 		onInit: function() {
 			this.getView().bindElement("/Products('4711')");
-			this.getView().byId("idPrice2").bindElement("/Services('4711')");
 		}
 	});
 
