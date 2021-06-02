edf0dabfdabc47957da0240b037dd17dd771090b
wangzixi-diablo <cle.ee@163.com>
2021-06-02 10:35:30 +0800

    for my wechat blog, code is ready

--- odata-table/Table.controller.js
+++ odata-table/Table.controller.js
@@ -1,11 +1,22 @@
+// @ts-nocheck
 sap.ui.define([
 	"sap/ui/core/mvc/Controller"
 ], function(Controller) {
 	"use strict";
 
 	return Controller.extend("sap.ui.jerry.odatatable.table",{
+
+		counter: 0,
 		onInit: function() {
-			alert("hello");
+			this._trigger = new sap.ui.core.IntervalTrigger(1  * 100 );
+   			this._trigger.addListener(this.onRefreshTriggered, this);
 		},
+		onRefreshTriggered: function () {
+			var oBinding = this.byId("producttable").getBinding("items");
+			if( !!oBinding){
+				oBinding.refresh();
+				this.byId("counter").setText(this.counter++);
+			}
+		}
 	});
 });
