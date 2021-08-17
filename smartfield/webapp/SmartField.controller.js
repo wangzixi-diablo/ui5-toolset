cbf82d8c6fcfbb7165678b699150a3c36d7156be
wangzixi-diablo <cle.ee@163.com>
2021-08-17 20:57:35 +0800

    print current screen

--- smartfield/webapp/SmartField.controller.js
+++ smartfield/webapp/SmartField.controller.js
@@ -6,7 +6,13 @@ sap.ui.define([
 	return Controller.extend("sap.ui.demo.smartControls.SmartField", {
 		onInit: function() {
 			this.getView().bindElement("/Products('4711')");
-		}
+		},
+		onPrintTriggered : function(){
+			var sCtrlString = "width=500px,height=600px";
+			var oWin = window.open("","PrintWindow", sCtrlString);
+			oWin.document.write("<h1>Hello</h1>");
+			oWin.print();
+		  }
 	});
 
 });
