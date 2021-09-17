edf59ff53c7415d601612ab0a118d85e48fecab3
wangzixi-diablo <cle.ee@163.com>
2021-09-17 21:12:11 +0800

    no table data displayed

--- tabledelete/webapp/Table.controller.js
+++ tabledelete/webapp/Table.controller.js
@@ -5,7 +5,12 @@ sap.ui.define([
 
 	return Controller.extend("sap.ui.demo.tabledelete.Table", {
 		onInit: function() {
-			console.log("Jerry");
+			var oTable = this.byId("ins");
+			var sServiceUrl = "/here/goes/your/serviceUrl/";
+		//Adding service to the odata model
+			var oModel = new sap.ui.model.odata.ODataModel(sServiceUrl, true);
+		//Setting model to the table
+			oTable.setModel(oModel);
 		}
 	});
 
--- tabledelete/webapp/test/service/server.js
+++ tabledelete/webapp/test/service/server.js
@@ -21,6 +21,12 @@ sap.ui.define([
 			// simulate
 			var sPath = jQuery.sap.getModulePath("sap.ui.demo.tabledelete.test.service");
 			oMockServer.simulate(sPath + "/metadata.xml", sPath);
+			
+			oMockServer.attachAfter(sap.ui.core.util.MockServer.HTTPMETHOD.GET, function(oEvent) {
+				var oXhr = oEvent.getParameter("oXhr");
+				var aResultFiltered = [];
+				}
+			);
 
 			// start
 			oMockServer.start();
