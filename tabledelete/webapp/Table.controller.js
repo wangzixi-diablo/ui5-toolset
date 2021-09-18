a8b011c2619c6dde7fa0752a87d3d8d82d953581
wangzixi-diablo <cle.ee@163.com>
2021-09-18 09:49:15 +0800

    odata model works

--- tabledelete/webapp/Table.controller.js
+++ tabledelete/webapp/Table.controller.js
@@ -5,26 +5,26 @@ sap.ui.define([
 
 	return Controller.extend("sap.ui.demo.tabledelete.Table", {
 		onInit: function() {
-			var oTable = this.byId("ins");
+			var oTable = this.byId("jerrytable");
 			var sServiceUrl = "/here/goes/your/serviceUrl/";
 			oTable.oModel = new sap.ui.model.odata.ODataModel(sServiceUrl, true);
 			oTable.setModel(oTable.oModel);
 		},
 		addRow : function(oArg){
-			var oTable = this.byId("ins");
-
-			var oData = {
+			var oTable = this.byId("jerrytable");
+			var oNewProduct = {
 				ProductId: "999",
 				Name: "myProduct"
 			};
-			oTable.oModel.create("/Products", oData, {success: (oEvent)=> console.log("ok: ", oEvent), error: 
-			(oEvent) => console.log('fail: ', oEvent)});
+			oTable.oModel.create("/Products", oNewProduct, {success: ()=> alert("new Product created ok."), error: 
+			() => alert("Product creation failed.")});
 		},
 
 		deleteRow: function(oEvent){
-			var oTable = this.byId("ins");
-			oTable.oModel.remove(`/Products('999')`, {success: (oEvent)=> console.log("ok: ", oEvent), error: 
-			(oEvent) => console.log('fail: ', oEvent)});
+			var oTable = this.byId("jerrytable");
+			var oSelectedItem = oEvent.getSource().getBindingContext().getObject();
+			oTable.oModel.remove(`/Products('${oSelectedItem.ProductId}')`, {success: ()=> alert(`Product ${oSelectedItem.ProductId} deleted ok.`), error: 
+			() => alert(`Product ${oSelectedItem.ProductId} deleted failed.`)});
 		}
 	});
 });
