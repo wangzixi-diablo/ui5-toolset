9beb017038e90bc7515b44734a88562c87b7290b
wangzixi-diablo <cle.ee@163.com>
2021-09-18 08:37:37 +0800

    delete via odata can work:

--- tabledelete/webapp/Table.controller.js
+++ tabledelete/webapp/Table.controller.js
@@ -7,11 +7,24 @@ sap.ui.define([
 		onInit: function() {
 			var oTable = this.byId("ins");
 			var sServiceUrl = "/here/goes/your/serviceUrl/";
-		//Adding service to the odata model
-			var oModel = new sap.ui.model.odata.ODataModel(sServiceUrl, true);
-		//Setting model to the table
-			oTable.setModel(oModel);
+			oTable.oModel = new sap.ui.model.odata.ODataModel(sServiceUrl, true);
+			oTable.setModel(oTable.oModel);
+		},
+		addRow : function(oArg){
+			var oTable = this.byId("ins");
+
+			var oData = {
+				ProductId: "999",
+				Name: "myProduct"
+			};
+			oTable.oModel.create("/Products", oData, {success: (oEvent)=> console.log("ok: ", oEvent), error: 
+			(oEvent) => console.log('fail: ', oEvent)});
+		},
+
+		deleteRow: function(oEvent){
+			var oTable = this.byId("ins");
+			oTable.oModel.remove(`/Products('999')`, {success: (oEvent)=> console.log("ok: ", oEvent), error: 
+			(oEvent) => console.log('fail: ', oEvent)});
 		}
 	});
-
 });
