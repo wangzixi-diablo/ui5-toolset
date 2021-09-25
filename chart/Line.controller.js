fdcee87552aba6b35978e2144ee7e7833d446cac
wangzixi-diablo <cle.ee@163.com>
2021-09-25 22:43:07 +0800

    remove unused file

--- chart/Line.controller.js
+++ chart/Line.controller.js
@@ -1,12 +1,9 @@
 sap.ui.define([
         'sap/ui/core/mvc/Controller',
-        'sap/ui/model/BindingMode',
         'sap/ui/model/json/JSONModel',
-        'sap/viz/ui5/data/FlattenedDataset',
         'sap/viz/ui5/format/ChartFormatter',
-        'sap/viz/ui5/api/env/Format',
-        './InitPage'
-    ], function(Controller, BindingMode, JSONModel, FlattenedDataset, ChartFormatter, Format, InitPageUtil) {
+        'sap/viz/ui5/api/env/Format'
+    ], function(Controller, JSONModel, ChartFormatter, Format) {
     "use strict";
 
     var Controller = Controller.extend("sap.viz.sample.Line.Line", {
@@ -16,7 +13,6 @@ sap.ui.define([
         onInit : function (evt) {
             Format.numericFormatter(ChartFormatter.getInstance());
             var formatPattern = ChartFormatter.DefaultPattern;
-            // set explored app's demo model on this sample
             
             var oVizFrame = this.oVizFrame = this.getView().byId("jerryFrame");
             oVizFrame.setVizProperties({
@@ -50,11 +46,7 @@ sap.ui.define([
             var oPopOver = this.getView().byId("idPopOver");
             oPopOver.connect(oVizFrame.getVizUid());
             oPopOver.setFormatString(formatPattern.STANDARDFLOAT);
-
-            InitPageUtil.initPageSettings(this.getView());
         }
     });
-
     return Controller;
-
 });
