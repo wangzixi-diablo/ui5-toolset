ff32125415f3e0b3a68cc6179e830fe710407fb3
wangzixi-diablo <cle.ee@163.com>
2022-03-09 21:19:32 +0800

    frame-ancestors 'none'

--- local.js
+++ local.js
@@ -16,7 +16,7 @@ var express = require('express');
 var app = express();
 
 app.use(function(req, res, next) {
-    res.setHeader("Content-Security-Policy", "script-src 'self' https://apis.google.com");
+    res.setHeader("Content-Security-Policy", "script-src 'self' https://apis.google.com;frame-ancestors 'none'");
     return next();
 });
 
